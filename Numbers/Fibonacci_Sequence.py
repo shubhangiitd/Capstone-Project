ca7d34e420f59c58c43ee86d91dd0cb9c5a26afc
shubhangiitd <70591597+shubhangiitd@users.noreply.github.com>
2020-09-01 22:01:41 +0530

    Update Fibonacci_Sequence.py

--- Numbers/Fibonacci_Sequence.py
+++ Numbers/Fibonacci_Sequence.py
@@ -2,7 +2,7 @@
 Enter a number and have the program generate the Fibonacci sequence to the Nth number.
 '''
 
-def fibonacci (n):
+def fibonacci(n):
     sequence = [1,1]
     for term in range(2,n):
         sequence.append(sequence[term-2]+sequence[term-1])
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     number = '0'
     while number.isdigit():
         
-        number = input("Please enter the number of digits (>1) you want in the Fibonacci sequence")
+        number = input("Please enter the number of digits (>1) you want in the Fibonacci sequence: ")
         
         try:
             print(fibonacci(int(number)))
