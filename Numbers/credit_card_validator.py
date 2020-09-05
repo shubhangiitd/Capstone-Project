f5e15b70030fcbec8341ebd6556513854faa2294
shubhangiitd <70591597+shubhangiitd@users.noreply.github.com>
2020-09-05 17:27:37 +0530

    Update credit_card_validator.py

--- Numbers/credit_card_validator.py
+++ Numbers/credit_card_validator.py
@@ -11,15 +11,17 @@ def credit_card_validator(number):
     if len(number) not in [15,16] or not number.isdigit():
         return 'Please enter all (15 or 16) digits of the credit card'
     
+    # Check digit is the last digit of the cc/dc number
     check_digit = number[-1]
+    
+    # Number other than check digit
     number_minus_check_digit = number[:len(number)-1]
     
-    #Reverse the string and take all odd and even position numbers 
+    # Reverse the string (doubling of digit starts from rightmost digit (next to check digit) and take all odd and even position numbers 
     num_at_odd_position = number_minus_check_digit[::-1][0::2]
     num_at_even_position = number_minus_check_digit[::-1][1::2]
     
-    #Double odd position number and add all digits of number
-    
+    # Double odd position number and add all digits of number. After that, take sum of all the numbers obtained
     sum_odd = 0
     for num in num_at_odd_position:
         if len(str(int(num)*2)) == 1:
@@ -27,11 +29,12 @@ def credit_card_validator(number):
         else:
             sum_odd += 1 + (int(num)*2)%10
             
+    # Sum of numbers at even position        
     sum_even = 0
     for num in num_at_even_position:
         sum_even += int(num)
 
-        
+    # sum of both odd and even positions obtained above is added. Remainder of sums by dividing by 10 should be equal to check digit  
     if 10 - ((sum_odd + sum_even) % 10) == int(check_digit):
         return 'Valid Card'
     
@@ -42,3 +45,6 @@ def credit_card_validator(number):
 def main():
         choice = input("Please enter credit card number: ")
         print(credit_card_validator(choice))
+        
+if __name__ == '__main__':
+    main()
